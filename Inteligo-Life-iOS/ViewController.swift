165afaf0dbe1f344843e2a00fb2f26d2bc71baa4
Fischer Tirado <fishdev@gmail.com>
2019-09-23 11:26:49 -0500

    Initial Commit

---
+++ Inteligo-Life-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Inteligo-Life-iOS
+//
+//  Created by fischer on 23/09/19.
+//  Copyright © 2019 Inteligo Bank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
